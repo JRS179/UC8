ad8ce2bf2d306a9511ed43e1e1cff62b49fee2fe
JRS179 <89670044+JRS179@users.noreply.github.com>
2021-09-18 16:08:43 -0300

    Add files via upload

---
+++ CAlunos.js
@@ -0,0 +1,19 @@
+const quantidadeAlunos = 14;
+
+for (let index = 0; index <= quantidadeAlunos; index++){
+    if (index ===0){
+        console.log ('o mumero é zero');
+        console.log()
+    }
+    else if(index % 2 ===0){
+        console.log('o numero',index, 'é par')
+        console.log()
+    }
+    else {
+        console.log('o numero',index, 'é impar')
+        console.log()
+    }
+    
+}
+console.log ('Deu certo!!!!!!') 
+console.log()
